7cad78332b6364f1f7ea2970951d4af76fe78207
Uzair Leo <uzairleo@Uzairs-MacBook-Air.local>
2021-09-17 14:31:45 +0500

    minor changes

---
+++ lib/audio-services.dart
@@ -0,0 +1,22 @@
+class AudioServices {
+//initializing all the instances of flutter_sound
+
+//defining methods for performing some sound operations in generic way
+/**
+ * record(),
+ * 
+ * play(),
+ * 
+ * stop(),
+ * 
+ * disposeRecorder(),
+ * 
+ * stopPlayer(),
+ * 
+ * 
+ * stopRecorder(),
+ * 
+ * 
+ */
+
+}
